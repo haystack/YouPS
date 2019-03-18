18dfd5ec294d0c2c6beadaee907296b90cc24c8d
Luke Murray <lukepigeonmail@gmail.com>
2019-03-18 18:20:05 -0400

    modified bash script

--- scripts/new_database.sh
+++ scripts/new_database.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+# make sure the user has update the database name in private.py
+read -p "Have you updated the database name in private.py? (yes/no): "
+if [ "$REPLY" != "yes" ]; then
+   exit
+fi
+
+# go to the root file
 cd ~/production/mailx || exit;
 
 # remove previous migrations
@@ -11,7 +19,6 @@ cd ~/production/mailx;
 echo -n Database Name:
 read databaseName
 echo 
-
 
 # get the mysql password
 echo -n MySql Password: 
@@ -36,6 +43,45 @@ python manage.py migrate schema
 # apply the djcelery migration
 python manage.py migrate djcelery
 
-echo -e "\e[33mMake sure to update the database name in private.py to \n \e[1m$databaseName"
+# alter tables to utf8
+mysql -u root -p$password <<EOF
+    ALTER TABLE
+        youps_folder
+        CONVERT TO CHARACTER SET utf8mb4
+        COLLATE utf8mb4_unicode_ci;
+
+    ALTER TABLE
+        youps_message
+        CONVERT TO CHARACTER SET utf8mb4
+        COLLATE utf8mb4_unicode_ci;
+
+    ALTER TABLE
+        youps_threads
+        CONVERT TO CHARACTER SET utf8mb4
+        COLLATE utf8mb4_unicode_ci;
+
+    ALTER TABLE
+        schema_mailbotmode
+        CONVERT TO CHARACTER SET utf8mb4
+        COLLATE utf8mb4_unicode_ci;
+
+    ALTER TABLE
+        schema_imapaccount
+        CONVERT TO CHARACTER SET utf8mb4
+        COLLATE utf8mb4_unicode_ci;
+
+    ALTER TABLE
+        schema_action
+        CONVERT TO CHARACTER SET utf8mb4
+        COLLATE utf8mb4_unicode_ci;
+
+    ALTER TABLE
+        youps_contact
+        CONVERT TO CHARACTER SET utf8mb4
+        COLLATE utf8mb4_unicode_ci;
+
+EOF
+
+echo -e "\e[33mMake sure to update the database name in private.py to \n \e[1m$databaseName\e[0m"
 
 
