beee1203e10609516a5da85802bd14fc4477ffaf
Luke Murray <lukepigeonmail@gmail.com>
2019-03-18 17:57:39 -0400

    wrote new database script

---
+++ scripts/new_database.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+cd ~/production/mailx || exit;
+
+# remove previous migrations
+cd schema/migrations || exit;
+rm -- !(__init__.*);
+cd ~/production/mailx;
+
+# get the name of the mysql database
+echo -n Database Name:
+read -s databaseName
+echo 
+
+
+# get the mysql password
+echo -n MySql Password: 
+read -s password
+echo
+
+# create the new database
+mysql -u root -p$password <<EOF
+    create database $databaseName;
+    grant all privileges ON $databaseName.* TO root@localhost;
+EOF
+
+# create the initial tables
+python manage.py syncdb;
+
+# create the initial schema migration with south
+python manage.py schemamigration schema --initial;
+
+# apply the schema migration
+python manage.py migrate schema
+
+# apply the djcelery migration
+python manage.py migrate djcelery
+
+echo -e "\e[33mMake sure to update the database name in private.py to \n \e[1m$databaseName"
+
+
