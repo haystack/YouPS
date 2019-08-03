3c1ac24f61708496796d9b9edbeb2a0c97611ab1
soyapark <soya@mit.edu>
2019-08-03 18:36:36 -0400

    Fix password reset

--- registration/forms.py
+++ registration/forms.py
@@ -18,7 +18,12 @@ from django.template import loader
 from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode
 from django.utils.translation import ugettext_lazy as _
+from django.conf import settings
 from http_handler.settings import WEBSITE, PRETEST_EMAIL, DEBUG
+
+import logging
+
+logger = logging.getLogger('youps')  # type: logging.Logger
 
 class RegistrationForm(forms.Form):
     """
@@ -118,12 +123,14 @@ class MurmurPasswordResetForm(PasswordResetForm):
     def save(self, domain_override=None,
              subject_template_name='registration/password_reset_subject.txt',
              email_template_name='registration/password_reset_email.html',
+             html_email_template_name=None,
              use_https=False, token_generator=default_token_generator,
              from_email=None, request=None):
         """
         Generates a one-use only link for resetting password and sends to the
         user.
         """
+        logger.debug("Send password reset form to user")
         from django.core.mail import send_mail
         UserModel = get_user_model()
         email = self.cleaned_data["email"]
@@ -153,5 +160,4 @@ class MurmurPasswordResetForm(PasswordResetForm):
             # Email subject *must not* contain newlines
             subject = ''.join(subject.splitlines())
             email = loader.render_to_string(email_template_name, c)
-            send_mail(subject, email, from_email, [user.email])
-
+            user.email_user(subject, email, settings.DEFAULT_FROM_EMAIL)
