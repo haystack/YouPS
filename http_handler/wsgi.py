18b9136513a92fdc191096771b13f58839342526
soyapark <soya@mit.edu>
2019-11-17 19:30:35 -0500

    Update wsgi loading

--- http_handler/wsgi.py
+++ http_handler/wsgi.py
@@ -20,10 +20,8 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "http_handler.settings")
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
-# from django.core.wsgi import get_wsgi_application
-# application = get_wsgi_application()
-import django.core.handlers.wsgi
-application = django.core.handlers.wsgi.WSGIHandler()
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
 # application = HelloWorldApplication(application)
